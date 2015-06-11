d31a194401520db53b1f133b0adebd949439ad72
Om Prakash Singh <oop.singh@gmail.com>
2015-06-11 21:59:04 +0530

    Add AP auto connect feature

--- app/src/main/java/com/nvidia/connectivity_test/WifiConnectActivity.java
+++ app/src/main/java/com/nvidia/connectivity_test/WifiConnectActivity.java
@@ -17,16 +17,17 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.TextView;
+import android.widget.ToggleButton;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-
 
 public class WifiConnectActivity extends ActionBarActivity {
 
@@ -40,15 +41,24 @@ public class WifiConnectActivity extends ActionBarActivity {
     private boolean disconnecting = false;
     private StringBuilder builder = new StringBuilder();
     private BroadcastReceiver mWifiStateReceiver;
-    private String TAG = "Connectivity-Test";
+    private ListView apList;
+    private ArrayList<String> SSID_list;
+    private TextView textStatus;
+    private EditText numCycles;
+    private ToggleButton mAutoConnectToggle;
+    private int autoConnectIndex = 0;
+    private int mAutoConnectCycles = 0;
+    private String TAG = "Connectivity-Test:AP-connect";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_wifi_connect);
-        final ListView apList = (ListView)findViewById(R.id.list_ap);
-        final TextView textStatus = (TextView) findViewById((R.id.text_connection_status));
-        final ArrayList<String> list = new ArrayList<String>();
+        apList = (ListView)findViewById(R.id.list_ap);
+        textStatus = (TextView) findViewById((R.id.text_connection_status));
+        mAutoConnectToggle = (ToggleButton) findViewById(R.id.toggleButton);
+        numCycles = (EditText) findViewById(R.id.numCycle);
+        SSID_list = new ArrayList<String>();
 
         mWifiManager = (WifiManager) this.getSystemService(WIFI_SERVICE);
 
@@ -61,7 +71,7 @@ public class WifiConnectActivity extends ActionBarActivity {
                     NetworkInfo nInfo = (NetworkInfo) intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
                     WifiInfo wifiInfo = mWifiManager.getConnectionInfo();
 
-                    //Same network selected. on disconnect start connection
+                    //Same network selected. on disconnect, start connection
                     if (nInfo.getState() == NetworkInfo.State.DISCONNECTED && disconnecting) {
                         disconnecting = false;
                         mWifiManager.enableNetwork(mNetId, true);
@@ -72,13 +82,25 @@ public class WifiConnectActivity extends ActionBarActivity {
                     }
 
                     if (mNetId == wifiInfo.getNetworkId() && connecting) {
+                        if (nInfo.getDetailedState() == NetworkInfo.DetailedState.CONNECTING) {
+                            textStatus.setText(builder.toString());
+                        }
+
                         if (nInfo.getDetailedState() == NetworkInfo.DetailedState.CONNECTED) {
                             timeMs = SystemClock.uptimeMillis() - startTime;
-                            builder.append("\nConnection done in ms: " + timeMs);
-                            builder.append("\nconnected to: ");
-                            builder.append(wifiInfo.getSSID());
-                            textStatus.setText(builder.toString());
+                            textStatus.setText("Connected to: " + wifiInfo.getSSID() + "in " + timeMs + " milliseconds ");
+                            Log.i(TAG, textStatus.getText().toString());
                             connecting = false;
+
+                            //auto connect
+                            if (mAutoConnectCycles == 0) {
+                                mAutoConnectToggle.setChecked(false);
+                                numCycles.setText("5");
+                                numCycles.setEnabled(true);
+                            } else {
+                                numCycles.setText(Objects.toString(mAutoConnectCycles));
+                            }
+                            handleAutoConnect();
                         }
                     }
                 }
@@ -90,11 +112,11 @@ public class WifiConnectActivity extends ActionBarActivity {
         for (WifiConfiguration c : configList) {
                if(c.status != WifiConfiguration.Status.DISABLED) {
                     mIdMap.put(c.SSID, c.networkId);
-                    list.add(c.SSID);
+                    SSID_list.add(c.SSID);
                }
         }
         final ArrayAdapter adapter = new ArrayAdapter(this,
-                android.R.layout.simple_list_item_1, list);
+                android.R.layout.simple_list_item_1, SSID_list);
         apList.setAdapter(adapter);
 
         if (mWifiManager.getConnectionInfo().getNetworkId() != -1) {
@@ -105,31 +127,80 @@ public class WifiConnectActivity extends ActionBarActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position,
                                     long id) {
-                String ssid = list.get(position);
-                for (Map.Entry<String, Integer> entry : mIdMap.entrySet()) {
-                    if (ssid.equals(entry.getKey())) {
-                        mNetId = entry.getValue();
-                        mSSID = ssid;
-                        if (mNetId == mWifiManager.getConnectionInfo().getNetworkId()) {
-                            builder = new StringBuilder();
-                            builder.append("Same network selected! Diconnecting...\n");
-                            textStatus.setText(builder.toString());
-                            disconnecting = true;
-                            mWifiManager.disconnect();
-                        } else {
-                            mWifiManager.enableNetwork(mNetId, true);
-                            startTime = SystemClock.uptimeMillis();
-                            connecting = true;
-                            builder = new StringBuilder();
-                            builder.append("Network selected: " + ssid);
-                            textStatus.setText(builder.toString());
-                        }
-                        break;
+                final int pos = position;
+                new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        apConnect(pos);
                     }
-                }
+                }).start();
             }
         });
+
+        //Pass extra parameter from command line to start auto toggle test
+        if (getIntent().getExtras()!= null) {
+            int cycles;
+            mAutoConnectCycles = getIntent().getIntExtra("cycles", 1);
+            Log.i(TAG, "starting auto connect test for " + mAutoConnectCycles + "cycle");
+            mAutoConnectToggle.setChecked(true);
+            numCycles.setText(Objects.toString(mAutoConnectCycles));
+            numCycles.setEnabled(false);
+            handleAutoConnect();
+        }
     } //end of onCreate
+
+    //Connect to ap in SSID_list of index pos
+    private void apConnect(int pos) {
+        String ssid = SSID_list.get(pos);
+        for (Map.Entry<String, Integer> entry : mIdMap.entrySet()) {
+            if (ssid.equals(entry.getKey())) {
+                mNetId = entry.getValue();
+                mSSID = ssid;
+                if (mNetId == mWifiManager.getConnectionInfo().getNetworkId()) {
+                    builder = new StringBuilder();
+                    builder.append("Same network selected! Disconnecting...\n");
+                    disconnecting = true;   //use it to connect to same network on DISCONNECTED event
+                    mWifiManager.disconnect();
+                } else {
+                    mWifiManager.enableNetwork(mNetId, true);
+                    startTime = SystemClock.uptimeMillis();
+                    connecting = true;
+                    builder = new StringBuilder();
+                    builder.append("Network selected: " + ssid);
+                }
+                break;
+            }
+        }
+    }
+
+    void handleAutoConnect() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                if (mAutoConnectToggle.isChecked() & mAutoConnectCycles > 0) {
+                    try {
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) { }
+                    apConnect(autoConnectIndex);
+                    autoConnectIndex = (++autoConnectIndex) % SSID_list.size();
+                    mAutoConnectCycles--;
+                }
+            }
+        }).start();
+    }
+
+    public void autoConnect(View view) {
+        boolean on = ((ToggleButton) view).isChecked();
+        if (on) {
+            mAutoConnectCycles = Integer.parseInt(numCycles.getText().toString());
+            numCycles.setEnabled(false);
+            autoConnectIndex = 0;
+            handleAutoConnect();
+        } else {
+            numCycles.setText("5");
+            numCycles.setEnabled(true);
+        }
+    }
 
     @Override
     protected void onDestroy(){
